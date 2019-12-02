c64c26c742b5add4665eb1984c54cbd61dad9678
ccaguirre <57292005+ccaguirre@users.noreply.github.com>
2019-12-01 19:31:55 -0600

    Update debugVariableDrills.py

--- debugVariableDrills.py
+++ debugVariableDrills.py
@@ -25,12 +25,19 @@ START HERE
 #name == "your name"
 #print(name)
 #Correct:
+name="your name"
+print(name)
+
 
 #2)
 #Broken:
 #x = "your name
 #print(x)
 #Correct:
+x="your name"
+print(x)
+
+
 
 #3)
 #Broken:
@@ -39,19 +46,22 @@ START HERE
 #PROBLEM: The variable is created correctly, but it won't print.
 #TIP: The variable isn't the problem
 #Correct:
-
+yay = "yay"
+print(yay)
 #4)
 #Broken:
 #y == 4
 #print(y)
 #Correct:
-
+y=4
+print(y)
 #5)
 #Broken:
 #m == 5
 #print(m)
 #Correct:
-
+m=5
+print(m)
 '''
 For the next section, there will be a variable create correctly. Your job will
 be to write what kind of variable it is.
@@ -71,39 +81,41 @@ START HERE
 #1)
 #x = 1.01
 #ANSWER:
+Float
 
 #2)
 #y = 2
 #ANSWER:
-
+Integer
 #3)
 #a = True
 #ANSWER:
-
+Boolean
 #4)
 #b = 1
 #ANSWER:
-
+Integer
 #5)
 #c = "string"
 #ANSWER:
-
+string
 #6)
 #d = False
 #ANSWER:
-
+Boolean
 #7)
 #e = ["a", "b", "c"]
 #ANSWER:
-
+List
 #8)
 #f = [1, 2, 3]
 #ANSWER:
-
+List
 #9)
 #g = "nope"
 #ANSWER:
-
+String
 #10)
 #h = 1.24
 #ANSWER:
+float
